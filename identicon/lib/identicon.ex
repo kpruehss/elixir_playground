0f8a00eed57f58156e9ed82f9365a0b3e2260ec9
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-11 17:18:12 +1000

    Identicon - Function @spec docs

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,16 @@ defmodule Identicon do
     Module to generate an Identicon based on a given string input (like your name)
   """
 
+  @doc """
+    The main pipeline. Supplying a string will generate a randomized Identicon based on the string's hash and save it to the root folder of the app
+  """
+  @spec main(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | byte,
+              binary | []
+            )
+        ) :: :ok | {:error, atom}
   def main(input) do
     input
     |> hash_input
@@ -14,10 +24,25 @@ defmodule Identicon do
     |> save_image(input)
   end
 
+  @doc """
+    Writes the image to file as png, using the string input as file name
+  """
+  @spec save_image(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | byte,
+              binary | []
+            ),
+          any
+        ) :: :ok | {:error, atom}
   def save_image(image, input) do
     File.write("#{input}.png", image)
   end
 
+  @doc """
+    Using Erlang's egd, turn pixel_map into 250x250px Identicon. First three integers from hash are used as RGB values
+  """
+  @spec draw_image(Identicon.Image.t()) :: binary
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
@@ -29,6 +54,10 @@ defmodule Identicon do
     :egd.render(image)
   end
 
+  @doc """
+    Take image struct and calculate pixel_map of all even squares
+  """
+  @spec build_pixel_map(Identicon.Image.t()) :: Identicon.Image.t()
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map =
       Enum.map(
@@ -47,6 +76,10 @@ defmodule Identicon do
     %Identicon.Image{image | pixel_map: pixel_map}
   end
 
+  @doc """
+    Remove odd-number squares from tuple. Only even squares will be colored
+  """
+  @spec filter_odd_squares(Identicon.Image.t()) :: Identicon.Image.t()
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     grid =
       Enum.filter(grid, fn {code, _index} ->
@@ -56,6 +89,10 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  @doc """
+    Generate grid tuples for later use by Erlang's EGD. Tuples are chunked to 3 items per list and flattened, then turned into a tuple with value-index pairing
+  """
+  @spec build_grid(Identicon.Image.t()) :: Identicon.Image.t()
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
       hex
@@ -67,15 +104,33 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  @doc """
+    Generate 5-square row based on 3-integer tuple. Values mirrored around 3rd integer in tuple
+  """
+  @spec mirror_row([...]) :: [...]
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
   end
 
+  @doc """
+    Use first three integers from hashed input to generate RGB values for Erlang's EDG
+  """
+  @spec pick_color(Identicon.Image.t()) :: Identicon.Image.t()
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
+  @doc """
+    Hash the input provided on startup into MD5 hash, turn into char-list
+  """
+  @spec hash_input(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | byte,
+              binary | []
+            )
+        ) :: Identicon.Image.t()
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
